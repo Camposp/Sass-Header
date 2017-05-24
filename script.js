f37ab7a87d7db217d53b33a0f985ac99585e0d5c
Elisa Campos <elisacampospousa@gmail.com>
2017-05-24 11:53:58 +0200

    exercise header responsive mobile first

---
+++ script.js
@@ -0,0 +1,17 @@
+var hamburguer = document.getElementsByClassName("btn-menu-stay")[0];
+
+function appearMenu() {
+
+  var menuHam = document.getElementsByClassName("menu")[0];
+
+  if (menuHam.style.display == "none"){
+
+      menuHam.style.display = "block";
+  }
+  else {
+
+      menuHam.style.display = "none";
+  }
+}
+
+hamburguer.addEventListener("click", appearMenu);
